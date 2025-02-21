7305798fa50c66f2db9738730157ccbee63cb2e0
ajeetraj11 <ajitpandey110920@gmail.com>
2025-02-21 13:46:38 +0530

    [Ajeet Raj] Add. Create Login Servlet Program

---
+++ Servlet-Introduction/src/main/loginservletpage/LoginServlet.java
@@ -0,0 +1,21 @@
+package loginservletpage;
+
+import utils.UserStore; // Import the UserStore class
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	   response.setContentType("text/html");
+	   PrintWriter out = response.getWriter();
+
+
+    }
+}
