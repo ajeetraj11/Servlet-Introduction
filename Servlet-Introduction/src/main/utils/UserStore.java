f987d6b27b262a0f4c1a35d1eba1e1e9d0ece9f6
ajeetraj11 <ajitpandey110920@gmail.com>
2025-02-21 13:51:07 +0530

    [Ajeet Raj] Add. Extend Servlet For Name

---
+++ Servlet-Introduction/src/main/utils/UserStore.java
@@ -0,0 +1,11 @@
+package utils;
+
+import java.util.HashMap;
+
+public class UserStore {
+
+    public static boolean isValidUserName(String username) {
+        return "Ajeet Raj".equals(username);
+    }
+
+}
